6806a4cb66be37bce53e6bbbc362b31003da44ea
Julian Pettersen <julian.alexander.pettersen@banenor.no>
2023-02-20 12:39:04 +0100

    Initial files

---
+++ pg6301_client/src/__tests__/app.test.tsx
@@ -0,0 +1,9 @@
+import {render, screen} from '@testing-library/react';
+import App from '../App';
+
+describe('App', () => { 
+    it('renders App component', () => {
+        // render(<App />);
+        expect(1 + 1 ).toBe(2);
+    });
+});
---
+++ pg6301_client/src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM, { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
+
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+
+root.render(
+  <React.StrictMode>
+    
+    <App />
+  </React.StrictMode>,
+)
